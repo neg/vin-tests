a2f9e202a28fb7ee5dd9e697c258c95ff5a16c5f
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2016-11-01 17:31:52 +0000

    h3-qv4l2: add qv4l2 run test
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

---
+++ h3-qv4l2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+base=$(readlink -f $(dirname $0))
+
+source $base/scripts/vin-tests.sh
+source $base/scripts/h3.sh
+
+media-ctl -r
+mc_set_link $csi40name 1 $vinname0 1
+mc_set_link $csi20name 1 $vinname1 1
+mc_set_link $csi20name 1 $vinname6 1
+
+# Need to get standard
+v4l2-ctl -d /dev/$vin1 --get-detected-standard
+v4l2-ctl -d /dev/$vin6 --get-detected-standard
+
+# Test HDMI
+test_qv4l2 $vin0
+
+# Test CVBS
+test_qv4l2 $vin1
+
+# Test dual CVBS
+test_qv4l2_dual $vin1 $vin6
+
+# Test dual CVBS+HDMI
+test_qv4l2_dual $vin1 $vin0
