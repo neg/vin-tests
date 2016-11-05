485e787e1c3d3fc4af678e4f9dd389ac12e9ab0b
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2016-11-05 10:09:10 +0000

    tools: vin_to_du: add helper script
    Simple script to help display the VIN on the DU. Works on both Gen2 and
    Gen3 hardware.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

---
+++ tools/vin_to_du.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+base=$(readlink -f $(dirname $0))
+
+if [ $# -ne 3 ]; then
+    echo "usage: $0 <V4L2 device> <Connector> <CRTC>"
+    echo "       $0 /dev/video25 66 64"
+    exit 1
+fi
+
+vdev=$1
+connector=$2
+crtc=$3
+
+# ADV7482 do not notify on format changes, preform manual probe
+case $(v4l2-ctl --list-inputs -d $vdev | awk '/Capabilities/{print $2};') in
+    "0x00000002")
+        v4l2-ctl --set-dv-bt-timings=query -d $vdev
+        ;;
+    "0x00000004")
+        std=$(v4l2-ctl --get-detected-standard -d $vdev | awk '/Video Standard/{print $4}')
+        v4l2-ctl --set-standard=$std -d $vdev
+        ;;
+    *)
+        echo "Unkown Capabilities"
+        exit 1
+        ;;
+esac
+
+# Figure out resolution, the DU is very pick about this, if it do not match perfectly
+# it will fail with a cryptic IOCTL -EINVAL error
+res=$(v4l2-ctl --get-fmt-video -d $vdev | awk '/Width/{sub(/\//, ",", $3); print $3};')
+
+$base/../src/dmabuf-sharing/dmabuf-sharing -M rcar-du -i $vdev -S $res -f YUYV -F YUYV -s $res@0,0 -t $res@0,0 -b 2 -o $connector:$crtc
