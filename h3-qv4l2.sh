c23e70ca1d4976cc1eca74b90573b33149d2374a
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2017-03-12 01:25:59 +0100

    h3: m3: update qv4l2 test for MC
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- h3-qv4l2.sh
+++ h3-qv4l2.sh
@@ -10,9 +10,9 @@ mc_set_link "$csi40name" 1 "$vinname0" 1
 mc_set_link "$csi20name" 1 "$vinname1" 1
 mc_set_link "$csi20name" 1 "$vinname6" 1
 
-# Need to get standard
-v4l2-ctl -d /dev/$vin1 --get-detected-standard
-v4l2-ctl -d /dev/$vin6 --get-detected-standard
+mc_propagate_format "$hdminame" 10 "$csi40name" 1 "$vinname0"
+mc_propagate_format "$cvbsname" 11 "$csi20name" 1 "$vinname1"
+mc_propagate_format "$cvbsname" 11 "$csi20name" 1 "$vinname6"
 
 # Test HDMI
 test_qv4l2 $vin0
