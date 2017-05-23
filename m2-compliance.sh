c59fd7691319831e18d6496503a430dd589ffb0e
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2017-05-23 16:13:33 +0200

    m2-compliance: set standard before running test
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- m2-compliance.sh
+++ m2-compliance.sh
@@ -7,7 +7,11 @@ source $base/scripts/m2.sh
 
 test_compliance_begin
 
+v4l2-ctl --set-dv-bt-timings=query -d /dev/$vin0
 test_compliance $vin0
+
+std=$(v4l2-ctl --get-detected-standard -d /dev/$vin1 | awk '/Video Standard/{print $4}')
+v4l2-ctl --set-standard=$std -d /dev/$vin1
 test_compliance $vin1
 
 # Make sure we can dequeue all buffers
