cba8a6a442613fdabc23a9bb985872b13b24c4de
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2017-09-27 18:39:12 +0100

    gmsl: Update gmsl helper to allow specifying a camera to capture
    abstract out the configuration such that we can iterate across the
    command line arguments as camera indexes.
    
    Each camera index will then be configured, and captured from in turn.
    
    It is also possible to capture from one camera multiple times, for example:
    
      ./gmsl.sh 1 2 8 1 1 2 8
    
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    [Niklas: small fixes per Kieran's instruction]
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- gmsl.sh
+++ gmsl.sh
@@ -7,74 +7,42 @@ source $base/scripts/boards.sh
 
 mdev=$(mc_get_mdev)
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <config>"
-    echo "config:"
-    echo "  1 - MAX9286 1 (CSI40)"
-    echo "  2 - MAX9286 2 (CSI41)"
-    echo "  3 - MAX9286 1 (CSI40) + MAX9286 2 (CSI41)"
-    exit 0
-fi
-
-max9286_1=0
-max9286_2=0
-
-case $1 in
-    1)
-        max9286_1=1
-        ;;
-    2)
-        max9286_2=1
-        ;;
-    3)
-        max9286_1=1
-        max9286_2=1
-        ;;
-    *)
-        echo "Unkown config '$1'"
-        exit 1
-        ;;
-esac
-
 out=$base/output
 rm -fr $out
 mkdir $out
 
+function capture() {
+    CSI="$1"
+    IDX="$2"
+    VIN="$3"
+    VID="$4"
+
+    mc_set_link "$CSI" $IDX "$VIN" 1
+    media-ctl -d $mdev -V "'$CSI':$IDX [fmt:UYVY8_2X8/1280x800 field:none]"
+    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/$VID-#.bin" /dev/${!VID}
+}
+
 mc_reset
 
-if [ $max9286_1 -eq 1 ]; then
-    mc_set_link "$csi40name" 1 "$vinname0" 1
-    mc_set_link "$csi40name" 2 "$vinname1" 1
-    mc_set_link "$csi40name" 3 "$vinname2" 1
-    mc_set_link "$csi40name" 4 "$vinname3" 1
+for cam in "$@"
+do
+	echo "Capturing camera $cam"
+	case $cam in
+	    1)	capture "$csi40name" 1 "$vinname0" vin0 ;;
+	    2)	capture "$csi40name" 2 "$vinname1" vin1 ;;
+	    3)	capture "$csi40name" 3 "$vinname2" vin2 ;;
+	    4)	capture "$csi40name" 4 "$vinname3" vin3 ;;
 
-    media-ctl -d $mdev -V "'$csi40name':1 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi40name':2 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi40name':3 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi40name':4 [fmt:UYVY8_2X8/1280x800 field:none]"
+	    5)	capture "$csi41name" 1 "$vinname4" vin4 ;;
+	    6)	capture "$csi41name" 2 "$vinname5" vin5 ;;
+	    7)	capture "$csi41name" 3 "$vinname6" vin6 ;;
+	    8)	capture "$csi41name" 4 "$vinname7" vin7 ;;
 
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin0-#.bin" /dev/$vin0
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin1-#.bin" /dev/$vin1
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin2-#.bin" /dev/$vin2
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin3-#.bin" /dev/$vin3
-fi
-
-if [ $max9286_2 -eq 1 ]; then
-    mc_set_link "$csi41name" 1 "$vinname4" 1
-    mc_set_link "$csi41name" 2 "$vinname5" 1
-    mc_set_link "$csi41name" 3 "$vinname6" 1
-    mc_set_link "$csi41name" 4 "$vinname7" 1
-
-    media-ctl -d $mdev -V "'$csi41name':1 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi41name':2 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi41name':3 [fmt:UYVY8_2X8/1280x800 field:none]"
-    media-ctl -d $mdev -V "'$csi41name':4 [fmt:UYVY8_2X8/1280x800 field:none]"
-
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin4-#.bin" /dev/$vin4
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin5-#.bin" /dev/$vin5
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin6-#.bin" /dev/$vin6
-    yavta -f YUYV -s 1280x800 -c10 --skip 7 --file="$out/vin7-#.bin" /dev/$vin7
-fi
+	    *)
+		echo "Unrecognised camera $cam"
+		;;
+	esac
+done;
 
 for f in $out/*bin; do
     name=$(basename $f .bin)
