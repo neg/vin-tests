0f2e7b6e15b33fb10c78dcf7e0031280086276e5
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2017-11-29 14:46:46 +0000

    qv4l2: add support for devices with only CSI40
    V3M only have one CSI-2 instance, CSI40. Add support for this by
    checking if CSI20 information have been provided or not for the board
    qv4l2 is executed on.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- qv4l2.sh
+++ qv4l2.sh
@@ -16,15 +16,17 @@ case $gen in
     "gen3")
         mc_reset
         mc_set_link "$csi40name" 1 "$vinname0" 1
-        mc_set_link "$csi20name" 1 "$vinname1" 1
-        mc_set_link "$csi20name" 1 "$vinname6" 1
-
         mc_propagate_hdmi "$vinname0"
-        mc_propagate_cvbs "$vinname1"
-        mc_propagate_cvbs "$vinname6"
+
+        if [[ "$csi20name" != "" ]]; then
+            mc_set_link "$csi20name" 1 "$vinname1" 1
+            mc_propagate_cvbs "$vinname1"
+        fi
 
         test_qv4l2 $vin0
-        test_qv4l2 $vin1
+        if [[ "$csi20name" != "" ]]; then
+            test_qv4l2 $vin1
+        fi
         ;;
     *)
         echo "Unkown generation '$gen'"
