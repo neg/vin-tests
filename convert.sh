bb45f5e3f385d2d1ac3fc4f539628f371720eb86
Jacopo Mondi <jacopo@jmondi.org>
2018-05-11 12:00:39 +0200

    convert: Accept destination format as parameter
    Accept the destination image format as third parameter of the convert
    function.
    
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
    Acked-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- convert.sh
+++ convert.sh
@@ -2,6 +2,7 @@
 
 F=$(dirname $1)/$(basename $1 .bin)
 FRAME_SIZE=${2:-1280x720}
+FORMAT=${3:-RGB565}
 
-raw2rgbpnm -f RGB565 -s $FRAME_SIZE $F.bin $F.pnm
+raw2rgbpnm -f $FORMAT -s $FRAME_SIZE $F.bin $F.pnm
 convert $F.pnm $F.png
