ca9baa90bcc88c56d1caffa6e1615db7e7ef4372
Jacopo Mondi <jacopo@jmondi.org>
2020-06-15 14:19:33 +0200

    scripts/boards.sh: Fix M3-W identifier
    The M3-W SoC has been renamed to r8a77960, adjust the board.sh script to
    identify it correctly.
    
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- scripts/boards.sh
+++ scripts/boards.sh
@@ -14,6 +14,7 @@ case $info in
         csis="20 40 41"
         ;;
     "Renesas Salvator-X board based on r8a7796" | \
+    "Renesas Salvator-X board based on r8a77960" | \
         "Renesas Salvator-X 2nd version board based on r8a77965")
         gen="gen3"
         vins="0 1 2 3 4 5 6 7"
